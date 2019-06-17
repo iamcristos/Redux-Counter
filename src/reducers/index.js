61177f73fbfe7c419ddcf8384e14da01045d20cd
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-06-17 13:20:20 +0100

    -ft-completed increment by one -ft-completed decrement by one -ft-completed incrementIfOdd -ft-completed incrementAsync

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { INCREMENT, DECREMENT } from '../actions';
+import { INCREMENT, DECREMENT, INCREMENT_ODD } from '../actions';
 
 const initialState = {
   count: 0
@@ -13,8 +13,23 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      return {
+        ...state,
+        count: state.count + 1
+      }
     case DECREMENT:
     // Fill in the body of this case
+      return {
+        ...state,
+        count: state.count - 1
+      }
+    case INCREMENT_ODD:
+      if(state.count % 2 !== 0) {
+        return {
+          ...state,
+          count: state.count + 1
+        }
+      }
     default:
       return state;
   }
